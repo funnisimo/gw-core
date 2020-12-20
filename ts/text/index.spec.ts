15d1039148c0bc136e2dd751ed02ac86ed1e96b1
Funnisimo <funnisimo@gmail.com>
2020-12-20 09:11:13 -0600

    coverage for text/index

--- ts/text/index.spec.ts
+++ ts/text/index.spec.ts
@@ -1,11 +1,57 @@
-
+import 'jest-extended';
 import * as Text from './index';
 
 describe('text', () => {
 
-  test('configure', () => {
+  afterEach( () => {
+    Text.configure({
+      fg: null,
+      bg: null,
+      colorStart: 'Ω',
+      colorEnd: '∆',
+      field: '§'
+    });
+  });
+
+  test('exports', () => {
     expect(Text.configure).toBeDefined();
     expect(Text.compile).toBeDefined();
     expect(Text.eachChar).toBeDefined();
   });
+
+  test('default options', () => {
+    expect(Text.options).toBeObject();
+    expect(Text.options.defaultFg).toBeNull();
+    expect(Text.options.defaultBg).toBeNull();
+    expect(Text.options.colorStart).toEqual('Ω');
+    expect(Text.options.colorEnd).toEqual('∆');
+    expect(Text.options.field).toEqual('§');
+  });
+
+  test('configure', () => {
+    Text.configure({
+      fg: 'blue',
+      bg: 0x333,
+      colorStart: '!',
+      colorEnd: '#',
+      field: '$'
+    });
+
+    expect(Text.options.defaultFg).toEqual('blue');
+    expect(Text.options.defaultBg).toEqual(0x333);
+    expect(Text.options.colorStart).toEqual('!');
+    expect(Text.options.colorEnd).toEqual('#');
+    expect(Text.options.field).toEqual('$');
+  });
+
+  test('not configuring anything', () => {
+    Text.configure();
+    expect(Text.options).toBeObject();
+    expect(Text.options.defaultFg).toBeNull();
+    expect(Text.options.defaultBg).toBeNull();
+    expect(Text.options.colorStart).toEqual('Ω');
+    expect(Text.options.colorEnd).toEqual('∆');
+    expect(Text.options.field).toEqual('§');
+  });
+
 });
--- ts/text/index.ts
+++ ts/text/index.ts
@@ -14,7 +14,6 @@ import { wordWrap, splitIntoLines } from "./lines";
 import { addHelper, options } from "./config";
 
 interface Options {
-  helpers?: Record<string, Function>;
   fg?: any;
   bg?: any;
   colorStart?: string;
@@ -23,16 +22,10 @@ interface Options {
 }
 
 function configure(opts: Options = {}) {
-  if (opts.helpers) {
-    Object.entries(opts.helpers).forEach(([name, fn]) => {
-      addHelper(name, fn);
-    });
-  }
-
-  if (opts.fg) {
+  if (opts.fg !== undefined) {
     options.defaultFg = opts.fg;
   }
-  if (opts.bg) {
+  if (opts.bg !== undefined) {
     options.defaultBg = opts.bg;
   }
   if (opts.colorStart) {
