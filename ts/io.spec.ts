8b4d1578190637005a73a4eabba8ad405f4cd5cc
funnisimo <scastette@yahoo.com>
2021-01-19 22:50:56 +0000

    io.loop test

--- ts/io.spec.ts
+++ ts/io.spec.ts
@@ -1,12 +1,30 @@
+import 'jest-extended';
 import * as IO from "./io";
 
 describe("IO", () => {
-  test("addCommand", () => {
-    async function handler(_: IO.Event) {
-      return true;
-    }
+    test("addCommand", () => {
+        async function handler(_: IO.Event) {
+            return true;
+        }
 
-    IO.addCommand("test", handler);
-    expect(IO.commands.test).toBe(handler);
-  });
+        IO.addCommand("test", handler);
+        expect(IO.commands.test).toBe(handler);
+    });
+
+
+    test('loop', async () => {
+        const handler = jest.fn().mockResolvedValue(true);
+        const keymap = {
+            a: handler
+        };
+
+        const result = IO.loop(keymap);
+        expect(handler).not.toHaveBeenCalled();
+
+        const ev = IO.makeKeyEvent({ key: 'a', code: 'KEY_A' } as KeyboardEvent);
+        IO.pushEvent(ev);
+
+        await result;
+        expect(handler).toHaveBeenCalledWith(ev);
+    });
 });
