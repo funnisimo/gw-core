1489f1933d7e99a38e53eb92993a2d3bd70468e2
Funnisimo <scastette@yahoo.com>
2021-11-15 09:17:10 -0600

    manual update of gw-utils.d.ts for rollup-plugin-dts issue #185

--- docs/js/gw-utils.js
+++ docs/js/gw-utils.js
@@ -2,7 +2,7 @@
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.GWU = {}));
-})(this, (function (exports) { 'use strict';
+}(this, (function (exports) { 'use strict';
 
     /**
      * GW.utils
@@ -7244,5 +7244,5 @@ void main() {
 
     Object.defineProperty(exports, '__esModule', { value: true });
 
-}));
+})));
 //# sourceMappingURL=gw-utils.js.map
